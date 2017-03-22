4fada58314625be65047b86e3d020e7ac277ba5f
mananshah1 <mananshahgujarat@gmail.com>
2017-03-22 05:45:51 -0700

    Update twitter sanders.R

--- twitter sanders.R
+++ twitter sanders.R
@@ -3,10 +3,10 @@ library(ROAuth)
 library(httr)
 
 # Set API Keys
-api_key <- "BBzrGIvtm8Roh5hZUUEeaE97X"
-api_secret <- "2ATtfVz5hptTXJMeBciZ13W7Vo4Wjt6dSlkZoQgLsjgNPAa0sh"
-access_token <- "776987256208756736-lbweNhSjJVH2N7wTHl9BEUCa0xo1fg6"
-access_token_secret <- "6uKvkgKpgBqqXwjFoMzAbEhtJrBVjMh11WILm5XIBTtxh"
+api_key <- a
+api_secret <-  b
+access_token <- c
+access_token_secret <-  d
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
 
